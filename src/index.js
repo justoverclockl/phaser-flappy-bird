4c67b5711a11f542d17efb51a1b5833280c86038
Marco Colia <marco.colia@fiftydeg.com>
2023-10-23 15:38:49 +0200

    refactored into class

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 import Phaser from 'phaser'
+import PlayScene from "./scenes/PlayScene";
+
+const WIDTH = 800;
+const HEIGHT = 600;
+const BIRD_POSITION = { x: WIDTH * 0.1, y: HEIGHT / 2 }
+
+const SHARED_CONFIG = {
+    width: WIDTH,
+    height: HEIGHT,
+    startPosition: BIRD_POSITION
+}
 
 const config = {
     type: Phaser.AUTO,
@@ -10,123 +21,10 @@ const config = {
             debug: true,
         }
     },
-    scene: {
-        preload,
-        create,
-        update,
-    }
+    scene: [new PlayScene(SHARED_CONFIG)]
 }
 
-const {width, height} = config;
 
-const PIPES_TO_RENDER = 4;
-
-let bird = null
-
-let pipes = null
-
-const pipeVerticalDistanceRange = [100, 250];
-const horizontalDistanceRange = [500, 550];
-let pipeHorizontalDistance = 0;
-
-
-const flapVelocity = 200
-const initialBirdPosition = { x: width * 0.1, y: height / 2 }
-
-// loading assets - images animation etc
-function preload() {
-    // this context is a scene that contains
-    // func and props we can use
-    this.load.image('sky', 'assets/sky.png')
-    this.load.image('bird', 'assets/bird.png')
-    this.load.image('obstacle', 'assets/pipe.png')
-}
-
-//
-function create() {
-    // first value = x, second is y, third is the key of the img
-    this.add
-        .image(0, 0, 'sky')
-        .setOrigin(0, 0) // set to the correct entry point of the canvas
-
-    pipes = this.physics.add.group()
-
-    for (let i = 0; i < PIPES_TO_RENDER; i++) {
-
-        const upperPipe = pipes.create(0,0, 'obstacle')
-            .setOrigin(0,1)
-
-        const lowerPipe = pipes.create(0,0, 'obstacle')
-            .setOrigin(0,0)
-
-        placePipe(upperPipe, lowerPipe)
-    }
-
-    pipes.setVelocityX(-200)
-
-    bird = this.physics.add
-        .sprite(width / 10, height / 2, 'bird')
-        .setOrigin(0)
-    bird.body.gravity.y = 400
-
-    const spaceBar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
-    spaceBar.on('down', fly)
-}
-
-function update() {
-    if (bird.y < 0 - bird.height || bird.y > height ) {
-        restartBirdPosition()
-    }
-
-    recyclePipes()
-}
-
-function placePipe(uPipe, lPipe) {
-
-    const rightMostX = getRightMostPipe()
-    const pipeVerticalDistance = Phaser.Math.Between(...pipeVerticalDistanceRange)
-    const pipeVerticalPosition = Phaser.Math.Between(0 + 20, height - 20 - pipeVerticalDistance)
-    const pipeHorizontalDistance = Phaser.Math.Between(...horizontalDistanceRange)
-
-    uPipe.x = rightMostX + pipeHorizontalDistance
-    uPipe.y = pipeVerticalPosition
-
-    lPipe.x = uPipe.x
-    lPipe.y = uPipe.y + pipeVerticalDistance
-}
-
-function recyclePipes() {
-    const tempPipes = []
-    pipes.getChildren().forEach((pipe) => {
-        if (pipe.getBounds().right < 0) {
-            tempPipes.push(pipe)
-
-            if (tempPipes.length === 2) {
-                placePipe(...tempPipes)
-            }
-        }
-    })
-}
-
-function getRightMostPipe() {
-    let rightMostX = 0
-
-    pipes.getChildren().forEach((pipe) => {
-        rightMostX = Math.max(pipe.x, rightMostX)
-    })
-
-    return rightMostX
-}
-
-function restartBirdPosition() {
-    bird.x = initialBirdPosition.x
-    bird.y = initialBirdPosition.y
-    bird.body.velocity.y = 0
-}
-
-function fly() {
-    bird.body.velocity.y = -flapVelocity
-}
 
 new Phaser.Game(config)
 
---
+++ src/scenes/PlayScene.js
@@ -0,0 +1,143 @@
+import Phaser from 'phaser'
+
+const PIPES_TO_RENDER = 4;
+
+class PlayScene extends Phaser.Scene {
+    constructor(config) {
+        super('PlayScene');
+        this.config = config
+
+        this.bird = null;
+        this.pipes = null;
+        this.pipeHorizontalDistance = 0;
+        this.pipeVerticalDistanceRange = [100, 250];
+        this.horizontalDistanceRange = [500, 550];
+        this.flapVelocity = 200;
+    }
+
+    preload() {
+        this.load.image('sky', 'assets/sky.png')
+        this.load.image('bird', 'assets/bird.png')
+        this.load.image('obstacle', 'assets/pipe.png')
+        this.load.image('gameover', 'assets/gameover.png')
+    }
+
+    create() {
+        this.createBg()
+        this.createBird()
+        this.createPipes()
+        this.createColliders()
+        this.handleInputs()
+    }
+
+    update() {
+        this.checkGameStatus()
+        this.recyclePipes()
+    }
+
+    createBg() {
+        this.add
+            .image(0, 0, 'sky')
+            .setOrigin(0, 0)
+    }
+
+    createBird() {
+        this.bird = this.physics.add
+            .sprite(this.config.startPosition.x, this.config.startPosition.y, 'bird')
+            .setOrigin(0)
+
+        this.bird.body.gravity.y = 400
+        this.bird.setCollideWorldBounds(true)
+    }
+
+    createPipes() {
+        this.pipes = this.physics.add.group()
+
+        for (let i = 0; i < PIPES_TO_RENDER; i++) {
+            const upperPipe = this.pipes
+                .create(0, 0, 'obstacle')
+                .setImmovable(true)
+                .setOrigin(0, 1)
+            const lowerPipe = this.pipes
+                .create(0, 0, 'obstacle')
+                .setImmovable(true)
+                .setOrigin(0, 0)
+
+            this.placePipe(upperPipe, lowerPipe)
+        }
+
+        this.pipes.setVelocityX(-200)
+    }
+
+    createColliders() {
+        this.physics.add.collider(this.bird, this.pipes, this.gameOver, null, this)
+    }
+
+    handleInputs() {
+        this.input.on('pointerdown', this.fly, this)
+        const spacebar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+        spacebar.on('down', this.fly, this)
+    }
+
+    checkGameStatus() {
+        if (this.bird.getBounds().bottom >= this.config.height || this.bird.y <= 0 ) {
+            this.gameOver()
+        }
+    }
+
+    placePipe(uPipe, lPipe) {
+        const rightMostX = this.getRightMostPipe()
+        const pipeVerticalDistance = Phaser.Math.Between(...this.pipeVerticalDistanceRange)
+        const pipeVerticalPosition = Phaser.Math.Between(0 + 20, this.config.height - 20 - pipeVerticalDistance)
+        const pipeHorizontalDistance = Phaser.Math.Between(...this.horizontalDistanceRange)
+
+        uPipe.x = rightMostX + pipeHorizontalDistance
+        uPipe.y = pipeVerticalPosition
+
+        lPipe.x = uPipe.x
+        lPipe.y = uPipe.y + pipeVerticalDistance
+    }
+
+    getRightMostPipe() {
+        let rightMostX = 0
+
+        this.pipes.getChildren().forEach((pipe) => {
+            rightMostX = Math.max(pipe.x, rightMostX)
+        })
+
+        return rightMostX
+    }
+
+    recyclePipes() {
+        const tempPipes = []
+
+        this.pipes.getChildren().forEach((pipe) => {
+            if (pipe.getBounds().right < 0) {
+                tempPipes.push(pipe)
+
+                if (tempPipes.length === 2) {
+                    this.placePipe(...tempPipes)
+                }
+            }
+        })
+    }
+
+    gameOver() {
+        this.physics.pause()
+        this.bird.setTint(0xff0000)
+
+        this.time.addEvent({
+            delay: 2000,
+            callback: () => {
+              this.scene.restart()
+            },
+            loop: false
+        })
+    }
+
+    fly() {
+        this.bird.body.velocity.y = -this.flapVelocity
+    }
+}
+
+export default PlayScene;
